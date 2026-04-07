facf128d64952bf4bfd1e826ee12bf57a4c4f53a
sorovsk <294677208@qq.com>
2026-04-07 15:04:28 +0800

    Add solution for rmsprop-optimizer

---
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
+    """
+    Perform one RMSProp update step.
+    """
+    # Write code here
+    # step 0
+    w, g, s = np.array(w), np.array(g), np.array(s)
+    # step 1
+    s_t = beta * s + (1 - beta) * g ** 2
+    # step 2
+    w_t = w - lr / np.sqrt(s_t + eps) * g
+    
+    return w_t, s_t
