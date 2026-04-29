77572c977f4fa19185b90c2fc521e2c678e2390b
sorovsk <294677208@qq.com>
2026-04-29 16:20:09 +0800

    Update solution for percent-change

--- percent-change/percent-change.py
+++ percent-change/percent-change.py
@@ -6,9 +6,7 @@ def percent_change(series):
     n = len(series)
     ret = [0.0] * (n - 1)
     for i in range(1, n):
-        if series[i - 1] == 0:
-            ret[i - 1] = 0
-        else:
-            ret[i - 1] = (series[i] - series[i - 1]) / series[i - 1]
+        pre, cur = series[i - 1], series[i]
+        ret[i - 1] = 0 if pre == 0 else (cur - pre) / pre 
     return ret
         
