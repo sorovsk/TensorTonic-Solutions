c5374f1d868a4e25bbe6b276af5c0f78148d65aa
sorovsk <294677208@qq.com>
2026-05-07 10:08:25 +0800

    Update solution for q-learning-update

--- q-learning-update/q-learning-update.py
+++ q-learning-update/q-learning-update.py
@@ -6,15 +6,7 @@ def q_learning_update(Q, s, a, r, s_next, alpha, gamma):
     """
     # Write code here
     # pass
-    Q_new = np.array(Q, dtype=float)
-    
-    # TD target = r + γ * max_a' Q(s_next, a')
-    td_target = r + gamma * np.max(Q_new[s_next])
-    
-    # TD error = target - Q(s, a)
-    td_error = td_target - Q_new[s, a]
-    
-    # 更新 Q(s, a)
-    Q_new[s, a] += alpha * td_error
-
+    Q_new = np.array(Q, dtype = float)
+    update = r + gamma * np.max(Q_new[s_next]) - Q_new[s, a]
+    Q_new[s, a] += alpha * update
     return Q_new
