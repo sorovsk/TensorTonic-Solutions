834f08d1498c6edc86392dbe81edd7bac17d04dc
sorovsk <294677208@qq.com>
2026-05-09 17:20:07 +0800

    Add solution for edit-distance

---
+++ edit-distance/edit-distance.py
@@ -0,0 +1,20 @@
+def edit_distance(s1, s2):
+    """
+    Compute the minimum edit distance between two strings.
+    """
+    # dp 
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            cost = 0 if s1[i - 1] == s2[j - 1] else 1
+            dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost)
+            
+    return dp[m][n]
+                
+    
