73e95b1d01ee7c5630a2bbf2f758f7a698cc169a
sorovsk <294677208@qq.com>
2026-06-01 16:13:31 +0800

    Update solution for top-k-recommendations

--- top-k-recommendations/top-k-recommendations.py
+++ top-k-recommendations/top-k-recommendations.py
@@ -6,7 +6,11 @@ def top_k_recommendations(scores, rated_indices, k):
     unrated = [(scores[i], i) for i in range(len(scores)) if i not in rated_indices]
     
     # Sort by score descending
-    unrated.sort(key=lambda x: x[0], reverse=True)
+    unrated.sort(key = lambda x : x[0], reverse = True)
+
+    print (unrated)
     
     # Return indices of the top k (or all if fewer are available)
-    return [idx for _, idx in unrated[:k]]
+    return [i for _, i in unrated[:k]]
+
+top_k_recommendations([3.5,1.2,4.8,2.1,5], [0, 2], 2)
